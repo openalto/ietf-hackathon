9d5aa4177a9aba09dc776be9cb11bc8724968730
jensenzhang <hack@jensen-zhang.site>
2022-03-14 20:13:16 +0800

    Run rucio dev containers as miniet hosts
    Add a new mininet host cls `DynamicDocker` (`utils/node_ext.py`) to
    attach a running container as a mininet host. (see issue #6)
    
    `docker/docker-compose-with-rucio.yml` provides a simple demo.
    
    Signed-off-by: jensenzhang <hack@jensen-zhang.site>

---
+++ docker/build_docker_images.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t openalto/rucio-dev rucio-containers/dev/
+docker build -t openalto/xrootd rucio-containers/xrootd/
+
---
+++ utils/rucio_example.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+"""
+This is a simple example to demostrate rucio with containernet.
+"""
+from node_ext import DynamicDocker
+from mininet.net import Containernet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import info, setLogLevel
+setLogLevel('info')
+
+"""
+Create the following topology:
+
+              Rucio
+      1Mbps    |     5Mbps
+      25ms     |     25ms
+  s1 --------- s3 ----------- s4 -- XRD3
+  |            |              |
+  |            | 25ms         | 50ms
+  |            | 1Mbps        | 2Mbps
+  |            |              |
+ XRD1          s2 -- XRD2     s5 -- XRD4
+"""
+
+net = Containernet(controller=Controller)
+info('*** Adding controller\n')
+net.addController('c0')
+
+info('*** Adding docker containers\n')
+rucio = net.addHost('rucio', ip='10.0.0.250', cls=DynamicDocker)
+xrd1 = net.addHost('xrd1', ip='10.0.0.251', cls=DynamicDocker)
+xrd2 = net.addHost('xrd2', ip='10.0.0.252', cls=DynamicDocker)
+xrd3 = net.addHost('xrd3', ip='10.0.0.253', cls=DynamicDocker)
+xrd4 = net.addHost('xrd4', ip='10.0.0.254', cls=DynamicDocker)
+
+info('*** Adding switches\n')
+s1 = net.addSwitch('s1')
+s2 = net.addSwitch('s2')
+s3 = net.addSwitch('s3')
+s4 = net.addSwitch('s4')
+s5 = net.addSwitch('s5')
+
+info('*** Creating links\n')
+net.addLink(xrd1, s1)
+net.addLink(xrd2, s2)
+net.addLink(rucio, s3)
+net.addLink(s1, s3, cls=TCLink, delay='25ms', bw=1)
+net.addLink(s2, s3, cls=TCLink, delay='25ms', bw=1)
+net.addLink(xrd3, s4)
+net.addLink(s3, s4, cls=TCLink, delay='25ms', bw=2)
+net.addLink(xrd4, s5)
+net.addLink(s4, s5, cls=TCLink, delay='50ms', bw=5)
+info('*** Starting network\n')
+net.start()
+info('*** Testing connectivity\n')
+net.ping([rucio, xrd1, xrd2, xrd3, xrd4])
+info('*** Running CLI\n')
+CLI(net)
+info('*** Stopping network')
+net.stop()
+
